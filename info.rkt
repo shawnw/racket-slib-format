da82388661058a2a61cb8d8a2c55e9fa25bb6c2e
Shawn Wagner <shawnw.mobile@gmail.com>
2024-07-21 12:37:26 -0700

    Add sandbox-lib to build dependencies.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection 'multi)
 (define deps '("base" "soup-lib"))
-(define build-deps '("scribble-lib" "racket-doc"))
+(define build-deps '("scribble-lib" "racket-doc" "sandbox-lib"))
 (define pkg-desc "format from SLIB ported to Racket")
 (define version "3.1.1")
 (define pkg-authors '(shawnw))
